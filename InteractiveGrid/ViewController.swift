02eb84d1bd7e8970bc0401ed7fb3754e7db5ed94
Chandler <Chandler@chandlers-mbp.attlocal.net>
2017-04-21 14:35:16 -0400

    Interactive Grid

--- InteractiveGrid/ViewController.swift
+++ InteractiveGrid/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let numberOfBoxesPerRow = 15
+    var cells = [String: UIView]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = view.frame.width / CGFloat(numberOfBoxesPerRow)
+        
+        for j in 0...30 {
+            for i in 0...numberOfBoxesPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.5
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
+                
+                let key = "\(i)|\(j)"
+                cells[key] = cellView
+                
+            }
+        }
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var selectedCell: UIView?
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        let width = view.frame.width / CGFloat(numberOfBoxesPerRow)
+        
+        let i = Int(location.x / width)
+        let j = Int(location.y / width)
+        
+        let key = "\(i)|\(j)"
+        
+        guard let cellView = cells[key] else {return}
+        view.bringSubview(toFront: cellView)
+        
+        if selectedCell != cellView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
+                
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+                
+            }, completion: nil)
+        }
+        
+        selectedCell = cellView
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            cellView.layer.transform = CATransform3DMakeScale(3, 3, 3)
+            
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+                UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: { 
+                    
+                    cellView.layer.transform = CATransform3DIdentity
+                    
+                }, completion: { (_) in
+                })
+        }
     }
-
-
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 }
 
